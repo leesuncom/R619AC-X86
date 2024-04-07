1df651fef4dc934e7d16651ba513a2656421586a
leesuncom <70786826+leesuncom@users.noreply.github.com>
2024-04-07 13:36:42 +0800

    Add files via upload

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -16,7 +16,7 @@ sed -i '/	refresh_config();/d' scripts/feeds
 echo "$(date +"%s")" >version.date
 sed -i '/$(curdir)\/compile:/c\$(curdir)/compile: package/opkg/host/compile' package/Makefile
 sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite --force-depends/' package/Makefile
-sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-app-firewall luci-app-opkg luci-app-upnp luci-app-autoreboot \
+sed -i "s/DEFAULT_PACKAGES:=/DEFAULT_PACKAGES:=luci-app-advancedplus luci-app-firewall luci-app-opkg luci-app-upnp luci-app-autoreboot \
 luci-app-wizard luci-base luci-compat luci-lib-ipkg luci-lib-fs \
 coremark wget-ssl curl autocore htop nano zram-swap kmod-lib-zstd kmod-tcp-bbr bash openssh-sftp-server block-mount resolveip ds-lite swconfig luci-app-fan /" include/target.mk
 sed -i "s/procd-ujail//" include/target.mk
@@ -32,36 +32,33 @@ while [[ "$status" == "in_progress" || "$status" == "queued" ]];do
 done
 
 
-mv -f feeds/kiddin9/r81* tmp/
+mv -f feeds/kiddin9/{r81*} tmp/
 
-sed -i "s/192.168.1.1/192.168.1.2/" package/feeds/kiddin9/base-files/files/bin/config_generate
-sed -i "s/192.168.1.1/192.168.1.2/" package/base-files/files/bin/config_generate
+sed -i "s/192.168.1/10.0.0/" package/feeds/kiddin9/base-files/files/bin/config_generate
+sed -i "s/192.168.1/10.0.0/" package/base-files/files/bin/config_generate
 
 #sed -i "/call Build\/check-size,\$\$(KERNEL_SIZE)/d" include/image.mk
+
+curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/kernel/linux/modules/video.mk -o package/kernel/linux/modules/video.mk
 
 git_clone_path master https://github.com/coolsnowwolf/lede target/linux/generic/hack-5.15
 curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/pending-5.15/613-netfilter_optional_tcp_window_check.patch -o target/linux/generic/pending-5.15/613-netfilter_optional_tcp_window_check.patch
 
-curl -sfL https://raw.githubusercontent.com/openwrt/openwrt/main/package/kernel/mac80211/realtek.mk -o package/kernel/mac80211/realtek.mk
-
 sed -i "s/CONFIG_WERROR=y/CONFIG_WERROR=n/" target/linux/generic/config-5.15
 
-# NeoBird
-# rm -rf feeds/kiddin9/luci-app-mosdns
-# git clone -b v5 https://github.com/sbwml/luci-app-mosdns.git feeds/kiddin9/luci-app-mosdns
-sed -i 's/^IMG_PREFIX\:\=.*/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)-$(shell TZ=UTC-8 date +"%Y.%m.%d-%H%M")-$(IMG_PREFIX_VERNUM)$(IMG_PREFIX_VERCODE)$(IMG_PREFIX_EXTRA)$(BOARD)$(if $(SUBTARGET),-$(SUBTARGET))/g' include/image.mk
-curl -sfL https://github.com/leesuncom/package/raw/main/99-default-settings -o feeds/kiddin9/my-default-settings/files/etc/uci-defaults/99-default-settings
-# NeoBird
+sed -i "s/no-lto,$/no-lto no-mold,$/" include/package.mk
 
-grep -q "23.05" include/version.mk && [ -d package/kernel/mt76 ] && {
+[ -d package/kernel/mt76 ] && {
 mkdir package/kernel/mt76/patches
 curl -sfL https://raw.githubusercontent.com/immortalwrt/immortalwrt/master/package/kernel/mt76/patches/0001-mt76-allow-VHT-rate-on-2.4GHz.patch -o package/kernel/mt76/patches/0001-mt76-allow-VHT-rate-on-2.4GHz.patch
-} || rm -rf devices/common/patches/mt7922.patch
-
-grep -q "1.8.8" package/network/utils/iptables/Makefile && {
-rm -rf package/network/utils/iptables
-git_clone_path openwrt-22.03 https://github.com/openwrt/openwrt package/network/utils/iptables
 }
+
+cd feeds/packages
+rm -rf libs/libpfring 
+git_clone_path master https://github.com/openwrt/packages libs/libpfring
+cd ../../
+
+rm -rf package/network/utils/xdp-tools package/feeds/kiddin9/quectel_MHI package/feeds/packages/v4l2loopback
 
 grep -q 'PKG_RELEASE:=9' package/libs/openssl/Makefile && {
 sh -c "curl -sfL https://github.com/openwrt/openwrt/commit/a48d0bdb77eb93f7fba6e055dace125c72755b6a.patch | patch -d './' -p1 --forward"
@@ -88,8 +85,6 @@ sed -i \
 	-e 's/+python\( \|$\)/+python3/' \
 	-e 's?../../lang?$(TOPDIR)/feeds/packages/lang?' \
 	package/feeds/kiddin9/*/Makefile
-
-rm -rf package/network/utils/xdp-tools package/feeds/packages/v4l2loopback
 
 (
 if [ -f sdk.tar.xz ]; then
